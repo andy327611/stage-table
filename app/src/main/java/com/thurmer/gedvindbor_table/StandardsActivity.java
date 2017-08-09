5f08bc4b1c5bd1179bce6aa43d2386d4cc07d63e
LAPTOP-0TIKE1GN\Xiang <xiang.chen.work@gmail.com>
2017-08-09 11:29:01 +0200

    Implemented listview with hardcoded standards

--- app/src/main/java/com/thurmer/gedvindbor_table/StandardsActivity.java
+++ app/src/main/java/com/thurmer/gedvindbor_table/StandardsActivity.java
@@ -1,13 +1,33 @@
 package com.thurmer.gedvindbor_table;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class StandardsActivity extends AppCompatActivity {
+    ArrayList<String> listItems = new ArrayList<String>();
+    ArrayAdapter<String> adapter;
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_standards);
+        listView = (ListView) findViewById(R.id.list);
+
+        adapter=new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1,
+                listItems);
+
+        listItems.add("M");
+        listItems.add("MF");
+        listItems.add("UNC");
+        listItems.add("UNF");
+
+        adapter.notifyDataSetChanged();
+        listView.setAdapter(adapter);
     }
 }
