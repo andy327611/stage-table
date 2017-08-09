f49144e269671c44586015cbb14e6d119059dd75
LAPTOP-0TIKE1GN\Xiang <xiang.chen.work@gmail.com>
2017-08-09 15:50:50 +0200

    Display selected standard into StandardDataActivity

--- app/src/main/java/com/thurmer/gedvindbor_table/StandardDataActivity.java
+++ app/src/main/java/com/thurmer/gedvindbor_table/StandardDataActivity.java
@@ -2,6 +2,7 @@ package com.thurmer.gedvindbor_table;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class StandardDataActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class StandardDataActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_standard_data);
+
+        TextView standardNameTextView = (TextView)findViewById(R.id.standardNameTextView);
+        standardNameTextView.setText(getIntent().getStringExtra("selectedStandard"));
     }
 }
